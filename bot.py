aa0af6e774d9f73d3ecbb83327f33d0de457c36f
deniskhvorov <Dzianis.Khvorau@ventionteams.com>
2026-03-02 11:06:50 +0300

    add stars

--- bot.py
+++ bot.py
@@ -52,6 +52,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 def _category_keyboard(include_help: bool = True) -> InlineKeyboardMarkup:
